13dde10df467b9c9b4048dc44fcca751965ca48b
Rselless <selles.rafael27@gmail.com>
2025-08-08 11:26:16 -0300

    feat: :sparkles: Inicial Footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,32 @@
-import React from "react";
-import Home from "./pages/home/Home";
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import './App.css'
+import Footer from './components/footer/Footer'
+import Navbar from './components/navbar/Navbar'
+import Home from './pages/home/Home'
+import ListaFuncionarios from './components/funcionario/listafuncionario/listaFuncionario'
+import FormFuncionario from './components/funcionario/formfuncionario/FormFuncionario'
+import DeletarFuncionario from './components/funcionario/deletarfuncionario/DeletarFuncionario'
 
 function App() {
-  return <Home />;
+  return (
+    <>
+        <BrowserRouter>
+          <Navbar />
+          <div className="min-h-[80vh]">
+         <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/cadastro" element={<FormFuncionario />} />
+            {/* <Route path="/login" element={<Login />} /> */}
+            <Route path="/funcionarios" element={<ListaFuncionarios />} />
+            <Route path="/cadastro" element={<FormFuncionario />} />
+            <Route path="/editar/:id" element={<FormFuncionario />} />
+            <Route path="/deletarfuncionario/:id" element={<DeletarFuncionario />} />
+         </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
+    </>
+  )
 }
 
-export default App;
+export default App
