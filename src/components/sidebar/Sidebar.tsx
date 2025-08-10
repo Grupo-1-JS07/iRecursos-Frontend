c1b624067a2cc0220b966909a68fad5b5cfd1578
Daniel <mocaibre@gmail.com>
2025-08-09 23:45:57 -0300

    alteração de baseurl

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,39 +1,68 @@
-import React from 'react';
-import { Calendar, GearSix, ListDashes, UserPlus, Users, House } from '@phosphor-icons/react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import {
+  Calendar,
+  GearSix,
+  ListDashes,
+  UserPlus,
+  Users,
+  House,
+} from "@phosphor-icons/react";
+import { Link } from "react-router-dom";
 
 const Sidebar: React.FC = () => {
   return (
     <div className="w-16 bg-white shadow-lg p-4 flex flex-col items-center">
       <nav>
         <ul>
-          <li className="mb-2"> {/* Dashboard */}
-            <Link to="/" className="flex items-center justify-center w-12 h-12 text-gray-600 hover:bg-blue-500 hover:text-white rounded-md transition-colors duration-200"> {/* Home */}
+          <li className="mb-2">
+            {" "}
+            {/* Dashboard */}
+            <Link
+              to="/dashboard"
+              className="flex items-center justify-center w-12 h-12 text-gray-600 hover:bg-blue-500 hover:text-white rounded-md transition-colors duration-200"
+            >
+              {" "}
+              {/* Home */}
               <House size={24} />
             </Link>
           </li>
           <li className="mb-2">
-            <Link to="/funcionarios" className="flex items-center justify-center w-12 h-12 text-gray-600 hover:bg-blue-500 hover:text-white rounded-md transition-colors duration-200">
+            <Link
+              to="/funcionarios"
+              className="flex items-center justify-center w-12 h-12 text-gray-600 hover:bg-blue-500 hover:text-white rounded-md transition-colors duration-200"
+            >
               <Users size={24} />
             </Link>
           </li>
           <li className="mb-2">
-            <Link to="/recrutamento" className="flex items-center justify-center w-12 h-12 text-gray-600 hover:bg-blue-500 hover:text-white rounded-md transition-colors duration-200">
+            <Link
+              to="/recrutamento"
+              className="flex items-center justify-center w-12 h-12 text-gray-600 hover:bg-blue-500 hover:text-white rounded-md transition-colors duration-200"
+            >
               <UserPlus size={24} />
             </Link>
           </li>
           <li className="mb-2">
-            <Link to="/performance" className="flex items-center justify-center w-12 h-12 text-gray-600 hover:bg-blue-500 hover:text-white rounded-md transition-colors duration-200">
+            <Link
+              to="/performance"
+              className="flex items-center justify-center w-12 h-12 text-gray-600 hover:bg-blue-500 hover:text-white rounded-md transition-colors duration-200"
+            >
               <ListDashes size={24} />
             </Link>
           </li>
           <li className="mb-2">
-            <Link to="/tempolivre" className="flex items-center justify-center w-12 h-12 text-gray-600 hover:bg-blue-500 hover:text-white rounded-md transition-colors duration-200">
+            <Link
+              to="/tempolivre"
+              className="flex items-center justify-center w-12 h-12 text-gray-600 hover:bg-blue-500 hover:text-white rounded-md transition-colors duration-200"
+            >
               <Calendar size={24} />
             </Link>
           </li>
           <li className="mb-2">
-            <Link to="/configuracoes" className="flex items-center justify-center w-12 h-12 text-gray-600 hover:bg-blue-500 hover:text-white rounded-md transition-colors duration-200">
+            <Link
+              to="/configuracoes"
+              className="flex items-center justify-center w-12 h-12 text-gray-600 hover:bg-blue-500 hover:text-white rounded-md transition-colors duration-200"
+            >
               <GearSix size={24} />
             </Link>
           </li>
--- src/services/Service.tsx
+++ src/services/Service.tsx
@@ -1,31 +1,43 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'http://localhost:4000/'
-})
+  baseURL: "https://irecursos-backend.onrender.com/",
+});
 
-export const cadastrarUsuario = async (url: string, dados: Object, setDados: Function) => {
-    const resposta = await api.post(url, dados)
-    setDados(resposta.data);
-}
+export const cadastrarUsuario = async (
+  url: string,
+  dados: Object,
+  setDados: Function
+) => {
+  const resposta = await api.post(url, dados);
+  setDados(resposta.data);
+};
 
 export const login = async (url: string, dados: Object, setDados: Function) => {
-    const resposta = await api.post(url, dados)
-    setDados(resposta.data);
-}
+  const resposta = await api.post(url, dados);
+  setDados(resposta.data);
+};
 
 export const buscar = async (url: string, setDados: Function) => {
-    const resposta = await api.get(url)
-    setDados(resposta.data)
-}
-export const cadastrar = async (url: string, dados: Object, setDados: Function) => {
-    const resposta = await api.post(url, dados)
-    setDados(resposta.data)
-}
-export const atualizar = async (url: string, dados: Object, setDados: Function) => {
-    const resposta = await api.put(url, dados)
-    setDados(resposta.data)
-}
+  const resposta = await api.get(url);
+  setDados(resposta.data);
+};
+export const cadastrar = async (
+  url: string,
+  dados: Object,
+  setDados: Function
+) => {
+  const resposta = await api.post(url, dados);
+  setDados(resposta.data);
+};
+export const atualizar = async (
+  url: string,
+  dados: Object,
+  setDados: Function
+) => {
+  const resposta = await api.put(url, dados);
+  setDados(resposta.data);
+};
 export const deletar = async (url: string) => {
-    await api.delete(url)
-}
+  await api.delete(url);
+};
